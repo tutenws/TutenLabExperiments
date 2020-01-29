8c1499a529a836827e9aa6a5afc7d9e600911475
Iona McLean <ionarmclean@gmail.com>
2020-01-29 08:40:13 -0800

    Start Coding Experiment Loop - Coded Grey Field Condition
    (button press issue fixed)

--- maskingExperiment.m
+++ maskingExperiment.m
@@ -36,23 +36,12 @@ expParams.GratingOscillationAmplitude = GetWithDefault('Oscillation Amplitude (P
 expParams.GratingOscillationFrequency_Hz = GetWithDefault('OscillationFrequency (Hz)', 4);
     %the oscillation frequency should  be randomized
 
-%...and so on as above
-
-%% TO DO
-%have to be able to move the circle around
-%make an option for a sine wave rather than a square wave.
-%oscillation frequency stair case?
-%participant responses preceed each presentation
-%click before each presentation
-%problem: changing the frequency does not seem to be changing the oscillation frequency
 
 %% Set up Psychtoolbox window
 
 %WINDOW PERAMETERS
 %Identify the Number of screens
 ScreenID = max(Screen('Screens')); %the largest number will be the screen that the stim is drawn to
-%ScreenID = min(Screen('Screens')); %the largest number will be the screen that the stim is drawn to
-  %for some reason using the max screen was not working
   
  %WHICH SCREEN TO DRAW ON
  [win]=Screen('OpenWindow',ScreenID); %window fills screen
@@ -68,7 +57,7 @@ expParams.SurroundH_dg=expParams.winYpx./expParams.displayPixelsPerDegree;
 [Xwincent, Ywincent] = RectCenter([0 0 expParams.winXpx expParams.winYpx]);
 
 
-%% Additional Stimulus & Experiment Variables
+%% Stimulus & Experiment Variables
 
 %TRIALS
 TrialsPerCond = 40;
@@ -78,11 +67,13 @@ TotalTrials = NumbOfCond.*TrialsPerCond;
 
 %Circle diameter in visual angle
 expParams.CircDiam_dg=7;
+expParams.TestSpotDiam_dg = 0.38;
 %Circle Luminance
 CircLum = [128,128,128];
 
 %STIMULUS VARIABLES IN PX
 CircDiam_px=expParams.displayPixelsPerDegree.*expParams.CircDiam_dg;
+TestSpotDiam_px = expParams.displayPixelsPerDegree.*expParams.TestSpotDiam_dg;
 
 %FIXATION LINE VARIABLES - 4 tick marks will be on the circle to direct the 
 %participant's gaze to the center of the circle 
@@ -94,6 +85,15 @@ FixLinePos2_px = (CircDiam_px./2)-FixLineLeangth_px;
 xCoords = [-FixLinePos1_px,-FixLinePos2_px,0,0,FixLinePos1_px,FixLinePos2_px,0,0];
 yCoords = [0,0,FixLinePos1_px,FixLinePos2_px,0,0,-FixLinePos1_px,-FixLinePos2_px];
 AllCoords = [xCoords; yCoords];
+
+%GREATING OSCILLATION
+ OneOcillation_sec = (1./expParams.GratingOscillationFrequency_Hz ); %time interval between each oscillation
+ 
+ %TRIAL SEQUENCE SPECIFICATIONS
+ dat.TrialDuration_s = 1.1;
+ dat.TestSpotDur_s = 0.1;
+ dat.PreTestSpotDur_s = 0.5;
+
 
 
 %% Preparation to Draw Grating
@@ -139,10 +139,7 @@ AdjustIncrement_px = 35;%30;
 
 %POSITION ADJUSTMENT INITATION
 PadResp = 0;
-FirstLoop = 0;
-% %GAME PAD CHECK LOOP
-% ButtonPress = 0;
-% KeyPresses = -1;
+
 while PadResp == 0 %while keys are not pressed
     %DRAW GREY RECTANGLE
     Screen('FillRect',win,[128,128,128]); %grey background
@@ -152,53 +149,30 @@ while PadResp == 0 %while keys are not pressed
     DrawFormattedText(win,'Stimulus Position Adjustment Procedure. Press any button to Continue','center','center',[],[],[],1);
     %1 - flips text across the horizontal axis so text looks right side up when projected to eye
     
+    Screen('DrawingFinished',win);
     Screen('Flip',win);
     
     %GAME PAD CHECK
-    if FirstLoop == 0
-        GamePad = GamePadInput([]);%must be inside the while loop for stimulus to work
-        FirstLoop = 1; %get out of first loop
-    end
+    GamePad = GamePadInput([]); %checks game pad
     if GamePad.buttonChange == 1 %any button pressed
         PadResp = 1; %out of while loop
-        
-%         LastPress = KeyPresses;
-%         KeyPresses = KeyPresses+1;
     end
-    
-%     %GAME PAD CHECK
-%     if KeyPress == ButtonPresses + 1 || ButtonPresses == 0
-%     GamePad = GamePadInput([]);%must be inside the while loop for stimulus to work
-%     
-%     if GamePad.buttonChange == 1 %any button pressed
-%         PadResp = 1; %out of while loop
-%     end
 end
 
-%ADJUSTING CIRCLE POSITION 
-%subject uses Y,B,X,A on the game pad to adjust the position of the circle
 
-%The subject should be presented with the grating and a a grey circle. They
-%should be able to move the position of the grating with the circle to
-%position it in the ceter. Then that position should be saved. The circle
-%will be moved by adjusting the center points to which the shapes are
+%ADJUSTING CIRCLE POSITION 
+%subject uses Y,B,X,A on the game pad to adjust the position of the circle. The circle
+%is be moved by adjusting the center points to which the shapes are
 %drawn. We want the grating to be lined up in the same way relative to the 
 %circle no matter the position of the circle. To do this the 
-%image is cropped so it appears the grating is moving with the circle.
+%image is cropped differently each time the circle moves so it appears the 
+%grating is moving with the circle.
 %*****There should eventually be a way to load previous locations of the circle 
 
-%basically you want to change the center to which everything is being drawn
 CircAdjustLoop = 0;
-FirstLoop=0;
-
 while CircAdjustLoop == 0 %adjusting the circle
-    GamePad = GamePadInput([]);
-    if FirstLoop == 0
-      %  GamePad = GamePadInput([]);
-        FirstLoop = 1;
-    end
     
-    %DRAW BAR SERIES 1 - series 1 just means that the white bar is first
+    %DRAW GRATING
     GratingPos_Txt = Screen('MakeTexture',win,GratingPos_Crop);%makes grating approapriate size
     Screen('DrawTexture',win,GratingPos_Txt); %draws the grating texture to the window
    
@@ -206,33 +180,30 @@ while CircAdjustLoop == 0 %adjusting the circle
     CircPos = CenterRectOnPointd([0, 0, CircDiam_px, CircDiam_px],Xpos,Ypos); %centers the circle on a point
     Screen('FillOval',win,CircLum,CircPos); %the circle is drawn in a box of the width and height of the circle
     
-    %DRAW FIXATION CROSS
+    %DRAW FIXATION LINES
     Screen('DrawLines',win,AllCoords,FixLineWidth_px,[0,0,0],[Xpos,Ypos]);
     
     %INSTRUCTIONS
-    DrawFormattedText(win,'Use buttons Y,B,X, and A to center the circle',(Xpos-(CircDiam_px./2)+20),(Ypos-40));
-    DrawFormattedText(win,'Once centered, press the right upper trigger',(Xpos-(CircDiam_px./2)+20),(Ypos+40));
+    DrawFormattedText(win,'Use buttons Y,B,X, and A to center the circle',(Xpos-(CircDiam_px./2)+20),(Ypos-60),[],[],[],1);
+    DrawFormattedText(win,'Once centered, press the right upper trigger',(Xpos-(CircDiam_px./2)+20),(Ypos+40),[],[],[],1);
     %corrdinates used to center the text within the circle
+    %1 - flips the text upside down
     
+    Screen('DrawingFinished',win); %all items drawn to screen. optimizes drawing.
     Screen('Flip', win);
     
-    %BUTTON PRESS --> CIRC POSITION ADJUSTMENT
-    %???How to make the button keep going when you hold the button down
-    %GamePad = GamePadInput([]);%intitates button check. Must be inside the while loop for stimulus to work
-    GamePad = GamePadInput([]);
+    %BUTTON CHECK & CIRC POSITION ADJUSTMENT
+    GamePad = GamePadInput([]); %checks game pad
     if GamePad.buttonChange == 1 %Game pad button press check
         if GamePad.buttonY == 1 %Y button pressed. Up movement of circ
             %CIRCLE POSITION
             Ypos = Ypos + AdjustIncrement_px; %note: it is flipped when on a window screen
-            %GRATING POSITION - adjust the way the grating is cropped
-            %you don't need to adjust this because the grating does not change vertically
+            %GRATING POSITION - you don't need to adjust this because the grating does not change vertically
         end
-        GamePad = GamePadInput([]);
         if GamePad.buttonA == 1 %A button pressed. Down movement
             %CIRCLE POSITION
             Ypos = Ypos - AdjustIncrement_px;
-            %GRATING POSITION - adjust the way the grating is cropped
-            %you don't need to adjust this because the grating does not change vertically
+            %GRATING POSITION - you don't need to adjust this because the grating does not change vertically
         end
         if GamePad.buttonX == 1 %X button press. leftward movement
             %CIRCLE ADJUSTMENT
@@ -253,32 +224,29 @@ while CircAdjustLoop == 0 %adjusting the circle
             xEndCropPos = xEndCropPos - AdjustIncrement_px;
             GratingPos_Crop = Grating(yStartCropPos:expParams.winYpx, xStartCropPos:xEndCropPos);
         end
-    end
-    
-    
-%     %ADJUSTMENT INSTRUCTIONS
-
-% 
-%     %DISPLAY TEXT - when completed
-%     DrawFormattedText(win,'Press the right upper trigger once circle is centered','center','center');
-    
-    %END ADJUSTMENT LOOP
-    GamePad = GamePadInput([]);%checks gamepad. must be inside the while loop for stimulus to work.
-    if GamePad.buttonRightUpperTrigger == 1
-        %END LOOP
-        CircAdjustLoop = 1;
-        
-        %RECALC CENTERED CIRCLE
-        CircPos = CenterRectOnPointd([0, 0, CircDiam_px, CircDiam_px],Xpos,Ypos); %centers the circle on a point
-
-        %SAVE ADJUSTMENT
-        dat.xStartGratingPos = xStartCropPos; %information for cropping grating
-        dat.xEndGratingPos = xEndCropPos;%information for cropping the grating
-        dat.CircXpos = Xpos; %x position of the circle
-        dat.CircYpos = Ypos; %y position of the circle
-    end
+ 
+        %END ADJUSTMENT LOOP
+        if GamePad.buttonRightUpperTrigger == 1
+            %END LOOP
+            CircAdjustLoop = 1;
+            
+            %SAVE ADJUSTMENT
+            dat.xStartGratingPos = xStartCropPos; %information for cropping grating
+            dat.xEndGratingPos = xEndCropPos;%information for cropping the grating
+            
+            dat.CircXpos = Xpos; %x position of the circle
+            dat.CircYpos = Ypos; %y position of the circle
+        end
+    end %game pad check
 end
-%% Given Position Adjustment Crop Grating & Determine oscillation
+
+%RECALCULATE CENTERED CIRCLE
+CircPos = CenterRectOnPointd([0, 0, CircDiam_px, CircDiam_px],Xpos,Ypos); %centers the circle on a point
+%RECALCULATE CENTER OF TEST SPOT
+TestSpotPos = CenterRectOnPointd([0, 0, TestSpotDiam_px, TestSpotDiam_px],Xpos,Ypos); %centers the test spot with the circle
+
+
+%% Prepare for Main Experiment - Grating Oscillation Set-up
 
 %DETERMINE GRATING SERIES 1 & 2
 %to create an oscillation between white and black bars, we will create two
@@ -290,25 +258,166 @@ Grating_Crop1 = GratingPos_Crop; %the crop of the grating chosen in the adjustme
 
 %GRATING 2
 %Determines how much to crop bar series 2 to simulate an oscillation of the specified period
-OscillationAmp = expParams.GratingOscillationAmplitude.*(2.*WidthOfOneBar_px);
-Grating_Crop2 = Grating(1:expParams.winYpx, OscillationAmp:(expParams.winXpx+OscillationAmp));
+OscillationAmp = expParams.GratingOscillationAmplitude.*(2.*WidthOfOneBar_px); %determines one oscilation cycle in pixels. Basically where to start the x crop of the second grating
+Grating_Crop2 = Grating(yStartCropPos:expParams.winYpx, (xStartCropPos+OscillationAmp):(xEndCropPos+OscillationAmp)); %crops the second bar series
+
+%% Prepare for Main Experiment - Condition Order
+%The conditions should be interleaved
+%Experimental Conditions: (1) Grey Field (2) Flickering grating at 0Hz,   
+%(3) 4Hz, (4) 10Hz, (5) 15Hz [these numbers will be used to label these conditions. 
+
+TrialArray = repelem([1,2,3,4,5],TrialsPerCond); %repeats numbers the number of trials per conditions
+dat.RandTrialOrder = TrialArray(randperm(length(TrialArray))); %shuffels numbers in the array
+
+%% Experimental loop
+
+%Experimental Conditions: (1) Grey Field (2) Flickering grating at 0Hz,   
+%(3) 4Hz, (4) 10Hz, (5) 15Hz [these numbers will be used to label these
+%conditions. Each conditoin will have 40 trials (40 staircases)
+
+%Experiment Progression: Grey background ask participant to initiate
+%proceedure. 500ms of grating condition 100ms of test spot presentation
+%followed by 500ms of grating condition again. Another grey screen asking
+%if participant saw the test spot "Yes" or "No". After response another
+%grey screen that asked participant to initiate next presentation. The
+%luminance of the test spot will be adjusted in each run using Quest to find 
+%the threshold for that condition.
+
+%BEGIN EXPERIMENT SCREEN
+FirstRun = 0; 
+while FirstRun == 0 %first run of experiment
+    %DRAW GREY RECTANGLE
+    Screen('FillRect',win,[128,128,128]); %grey background
+    
+    %DRAW TEXT
+    DrawFormattedText(win,'Main Experiment. Press any button to Continue','center','center',[],[],[],1);
+    %1 - flips text across the horizontal axis so text looks right side up when projected to eye
+    
+    Screen('DrawingFinished',win);
+    Screen('Flip',win);
+    
+    %GAME PAD CHECK
+    GamePad = GamePadInput([]); %checks game pad
+    if GamePad.buttonChange == 1 %any button pressed
+        FirstRun = 1; %out of while loop
+    end
+end
 
 
-%% Work on drawing stimulus - Iona
-   
+%EXPERIMENT LOOP
+ExptLoop = 0; 
+TrialCounter = 0; %first condition
+FirstLoop = 0;
+while ExptLoop == 0
+    
+    %DETERMINE NEXT CONDITION TYPE
+    TrialCounter = TrialCounter + 1; %determines the number of trials we have run
+    CondType = dat.RandTrialOrder(TrialCounter); %goes through the random condition array to choose the next condition type
+    
+    %GREY INTITIATION SCREEN - Subj initiates next trial
+    %the grey screen should be after each staircase so it should really be
+    %within the individual conditions
+    if FirstLoop = 1; %after the first trial
+    end
+    
 
     
- OneOcillation_sec = (1./expParams.GratingOscillationFrequency_Hz ); %time interval between each oscillation
+%%%%%%%%%%%%%%%%%% Grey Field Condition %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
+
+%******
+%replace this with the staircase
+TestSpotLum = [128,128,128];
+%******
+
+%TIME VARIABLES
+if CondType == 1
+    
+    %STIMULUS PRESENTATION
+    StartStim = GetSecs;
+    TimeNow = Startstim;
+    while TimeNow - StartStim < dat.TrialDuration_s %within trial duration
+        
+        %DRAW GREY RECTANGLE
+        Screen('FillRect',win,[128,128,128]); %grey background
+        
+        Screen('Flip', win);
+        
+        %PRESENT TEST SPOT
+        TimeNow = GetSecs;
+        while TimeNow < (StartStim + dat.PreTestSpotDur_s + dat.TestSpotDur_s) &&  TimeNow > (StartStim + dat.PreTestSpotDur_s)
+            
+            %DRAW GREY RECTANGLE
+            Screen('FillRect',win,[128,128,128]); %grey background
+            
+            %DRAW TEST SPOT
+            Screen('FillOval',win,TestSpotLum,TestSpotPos); %the circle is drawn in a box of the width and height of the circle
+            
+            Screen('Flip', win);
+            
+        end %test spot presentation
+    end %trial duration
+
+    
+%RESPONSE LOOP - subj indicates if they saw the test spot
+RespLoop = 1;
+while RespLoop == 1;
+    
+    %DRAW GREY RECTANGLE
+    Screen('FillRect',win,[128,128,128]); %grey background
+    
+    %DRAW TEXT
+    DrawFormattedText(win,'If you saw the test spot, press "Y". If you did not see the test spot press "A"','center','center',[],[],[],1);
+    %1 - flips text across the horizontal axis so text looks right side up when projected to eye
+    
+    %CHECK FOR BUTTON PRESS
+    GamePad = GamePadInput([]);
+    if GamePad.buttonChange == 1 %Game pad button press check
+        if GamePad.buttonY == 1 %subject saw the stimulus
+            RespLoop = 0; %end loop
+        end
+        if GamePad.buttonA == 1 %subject did not see the stimulus
+            RespLoop = 0; %end resp loop
+        end
+    end
+end %response loop
+    
+    
+%How do we know when the staircase has ended? 
+    
+% CondType = 0; %end condition loop
+end %Grey Field Cond
+
+
+
+
+
+
+if CondType == 2 %Grating No Flicker
+end %Grating No Flicker
+if CondType == 3 %Grating Flicker 4Hz
+end
+if CondType == 4 %Grating Flicker 10Hz
+end
+if CondType == 5 %Grating Flicker 15Hz
+end
+end %Experiment Loop
+
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 for i = 1:5 %change this later
     %OSCILLATING AT A FREQUENCY
     %beginning of experiment do I put the frequency?? or will It just
     %rotate through frequencies (look at paper) might need to be random
     %while thing with multiples of 2 - look at previous 
-StartTime = GetSecs;
-TimeNow = StartTime;
+StartStim = GetSecs;
+TimeNow = StartStim;
 LastChange = GetSecs; %records the time the grating was last oscilated
 %BAR SERIES 1
-while (TimeNow == StartTime) || (TimeNow >= OneOcillation_sec + LastChange)
+while (TimeNow == StartStim) || (TimeNow >= OneOcillation_sec + LastChange)
     %DRAW BAR SERIES 1
     Grating_Txt1 = Screen('MakeTexture',win,Grating_Crop1);
     Screen('DrawTexture',win,Grating_Txt1);
@@ -325,11 +434,11 @@ while (TimeNow == StartTime) || (TimeNow >= OneOcillation_sec + LastChange)
 end
 
 %TIME VARIABLES FOR BAR SERIES 2
-StartTime = GetSecs;
-TimeNow = StartTime;
+StartStim = GetSecs;
+TimeNow = StartStim;
 LastChange = GetSecs; %records the time the grating was last oscilated
 %BAR SERIES 2
-while (TimeNow == StartTime) || (TimeNow >= OneOcillation_sec + LastChange)
+while (TimeNow == StartStim) || (TimeNow >= OneOcillation_sec + LastChange)
     
     %DRAW BAR SERIES 2
     Grating_Txt2 = Screen('MakeTexture',win,Grating_Crop2);
