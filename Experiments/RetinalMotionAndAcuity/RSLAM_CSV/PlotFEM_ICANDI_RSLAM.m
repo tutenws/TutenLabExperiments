5281b2eb68058798fbf287c993250e606c292e34
alisabraun <63264366+alisabraun@users.noreply.github.com>
2022-09-27 17:15:06 -0700

    RSLAM excluded timepoints marked

--- Experiments/RetinalMotionAndAcuity/RSLAM_CSV/PlotFEM_ICANDI_RSLAM.m
+++ Experiments/RetinalMotionAndAcuity/RSLAM_CSV/PlotFEM_ICANDI_RSLAM.m
@@ -23,8 +23,8 @@ csvdata.YposZero = csvdata.YposZero.*-1;
 rslamdata = array2table(squeeze(rslam_dataAll.eye_motions(Rslam_VideoNumber,:,:)),'VariableNames',{'Timestamp', 'Xpos','Ypos'});
 rslamdata.XposZero = rslamdata.Xpos-rslamdata.Xpos(1);
 rslamdata.YposZero = rslamdata.Ypos-rslamdata.Ypos(1);
-%convert to arcmin
-csvdata.XposZeroArc = (csvdata.XposZero ./ avg_PPD) * 60;%% START HERE, look up PPD avg for all sessions
+% convert to arcmin
+csvdata.XposZeroArc = (csvdata.XposZero ./ avg_PPD) * 60;
 csvdata.YposZeroArc = (csvdata.YposZero ./ avg_PPD) * 60;
 rslamdata.XposZeroArc = (rslamdata.XposZero ./ avg_PPD) * 60;
 rslamdata.YposZeroArc = (rslamdata.YposZero ./ avg_PPD) * 60;
@@ -37,8 +37,27 @@ plot(csvdata.YposZero);
 legend('X pos','Y pos')
 title(['Icandy ' strrep(ICandy_CSV_file,'_','\_')]);
 subplot(2,1,2)
-plot(rslamdata.Timestamp, rslamdata.XposZero, 'g')
+plot(rslamdata.Timestamp, rslamdata.XposZero, '.-g');
 hold
-plot(rslamdata.Timestamp, rslamdata.YposZero);
+plot(rslamdata.Timestamp, rslamdata.YposZero, '.-r');
+hold
+% fill in missing datapoints
+% scatter(x,y);
+% x = [1,2,3,5,6,7,8,16,17,19,43];
+% y = [12,53,73,4,40,45,7,12,54,67,14];
+c = [0.7 0.7 0.7];
+idx = diff(rslamdata.Timestamp)>1;
+hold on;
+ctr = 1;
+for i=1:length(idx)
+    if idx(i)==1
+        X = [rslamdata.Timestamp(i) rslamdata.Timestamp(i+1) rslamdata.Timestamp(i+1) rslamdata.Timestamp(i)];
+        Y = [-60 -60 60 60];
+        fill(X,Y,c);hold on;
+    end
+    ctr = ctr + 1;
+end
 legend('X pos','Y pos')
+xlabel('Timestamp (total 1 second)')
+ylabel('Arcmin')
 title(sprintf('Rslam video %d in %s', Rslam_VideoNumber, strrep(rslam_output_file,'_','\_')));
